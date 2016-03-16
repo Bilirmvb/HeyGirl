a4ae2a9ffc06759e7e203335cbe4914a264fb49a
Formula <312948666@qq.com>
2016-03-16 10:16:40 +0800

    创建框架

---
+++ HeyHeyHey/HeyHeyHey/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeyHeyHey
+//
+//  Created by Formulator on 16/3/16.
+//  Copyright © 2016年 Formulator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
